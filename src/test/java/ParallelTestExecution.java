e0a1bda7028cbdd8ccbf5d8773d485801825cd6c
hidude111 <tan742@utexas.edu>
2025-12-22 00:38:02 -0600

    Updated ParallelTestExecution to be included in extent reporting.
    Upd testng.xml to be more customizable on what tests to run in parallel and which ones to run sequentially

--- src/test/java/ParallelTestExecution.java
+++ src/test/java/ParallelTestExecution.java
@@ -5,9 +5,13 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import static Utils.extentreports.ExtentTestManager.startTest;
+
+//To run this in parallel we need to use testng.xml, I will fix that later cause the tests in
 public class ParallelTestExecution {
     @Test
     public void testLoginInChrome() {
+        startTest("testLoginInChrome", "Parallel test to be ran with testLoginInEdge");
         WebDriver driver = new ChromeDriver();  // ChromeDriver setup
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
@@ -18,6 +22,7 @@ public class ParallelTestExecution {
 
     @Test
     public void testLoginInEdge() {
+        startTest("testLoginInEdge", "Parallel test to be ran with testLoginInChrome");
         WebDriver driver = new EdgeDriver();  // FirefoxDriver setup
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
